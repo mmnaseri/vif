dbb4f8d4c35151f77999bd272b637e9e0c3d17d3
Milad Naseri <mmnaseri@programmer.net>
2016-07-08 19:15:33 -0700

    update formatting

--- app/app.js
+++ app/app.js
@@ -1,5 +1,5 @@
 'use strict';
 angular.module('Vif', [
-  // load your modules here
-  'main', // starting with the main module
+	// load your modules here
+	'main', // starting with the main module
 ]);
